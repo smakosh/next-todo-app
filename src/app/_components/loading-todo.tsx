efc0bc0d3083a084c7af0c41502578eb38492f31
smakosh <ismai23l@hotmail.com>
2023-11-10 20:24:16 +0100

    chore: use noStore to enable dynamic rendering

--- src/app/_components/loading-todo.tsx
+++ src/app/_components/loading-todo.tsx
@@ -1,12 +1,15 @@
 import { Skeleton } from "@/components/ui/skeleton"
 
 const LoadingTodo = () => {
-  return [0, 1, 2].map((item) => (
-    <Skeleton
-      key={`todo-${item}`}
-      className="h-[100px] w-[100px] rounded-full"
-    />
-  ))
+  return (
+    <ul className="mt-4 space-y-2">
+      {[0, 1, 2, 4].map((item) => (
+        <li key={`todo-${item}`} className="flex flex-col space-y-2">
+          <Skeleton className="h-[100px] w-full rounded" />
+        </li>
+      ))}
+    </ul>
+  )
 }
 
 export default LoadingTodo
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Suspense } from "react"
+import { unstable_noStore } from "next/cache"
 
 import { prisma } from "@/lib/prisma"
 
@@ -7,12 +8,6 @@ import TodoForm from "./_components/todo-form"
 import TodoItem from "./_components/todo-item"
 
 export default async function Home() {
-  const todos = await prisma.todo.findMany({
-    orderBy: {
-      updatedAt: "desc",
-    },
-  })
-
   return (
     <div className="flex min-h-screen flex-col">
       <header className="flex items-center justify-center bg-blue-500 py-4 text-white">
@@ -22,22 +17,33 @@ export default async function Home() {
         <div className="mx-auto max-w-lg">
           <TodoForm />
           <Suspense fallback={<LoadingTodo />}>
-            {todos?.length > 0 ? (
-              <ul className="mt-4 space-y-2">
-                {todos.map(({ id, title, content, done }) => (
-                  <TodoItem
-                    key={id}
-                    id={id}
-                    done={done}
-                    content={content}
-                    title={title}
-                  />
-                ))}
-              </ul>
-            ) : null}
+            <Todos />
           </Suspense>
         </div>
       </main>
     </div>
   )
+}
+
+const Todos = async () => {
+  unstable_noStore()
+  const todos = await prisma.todo.findMany({
+    orderBy: {
+      updatedAt: "desc",
+    },
+  })
+
+  return todos?.length > 0 ? (
+    <ul className="mt-4 space-y-2">
+      {todos.map(({ id, title, content, done }) => (
+        <TodoItem
+          key={id}
+          id={id}
+          done={done}
+          content={content}
+          title={title}
+        />
+      ))}
+    </ul>
+  ) : null
 }
